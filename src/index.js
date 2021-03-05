f4256411e76d86ced4227259092fe30546f228f8
carlos <cgcarlosg@hotmail.com>
2021-03-05 11:05:46 -0500

    Add functions to add and save lists of projects in localstorage

--- src/index.js
+++ src/index.js
@@ -1,6 +1,34 @@
 const listsContainer = document.querySelector('[data-lists')
+const newListForm = document.querySelector('[data-new-list-form]')
+const newListInput= document.querySelector('[data-new-list-input]')
 
-let lists = [{id:1, name:'name'}, {id:2, name:'todo'}]
+const LOCAL_STORAGE_LIST_KEY = 'task.lists'
+let lists = JSON.parse(localStorage.getItem(LOCAL_STORAGE_LIST_KEY)) || []
+
+
+
+newListForm.addEventListener('submit', e => {
+  e.preventDefault()
+  const listName = newListInput.value
+  if (listName == null || listName === '') return
+  const list = createList(listName)
+  newListInput.value = null
+  lists.push(list)
+  render()
+})
+
+function createList(name) {
+ return { id: Date.now().toString(), name: name, tasks: [] }
+}
+
+function save() {
+  localStorage.setItem(LOCAL_STORAGE_LIST_KEY, JSON.stringify(lists))
+}
+
+function saveAndRender() {
+  save()
+  render()
+}
 
 function render() {
   clearElement(listsContainer)
