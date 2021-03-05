61a7bf9ad7f9786929bc3e0d7ab9d8af725bf39b
carlos <cgcarlosg@hotmail.com>
2021-03-05 10:04:39 -0500

    Change Style to index

--- dist/main.js
+++ dist/main.js
@@ -15,7 +15,7 @@
   \**********************/
 /***/ (() => {
 
-eval("const listsContainer = document.querySelector(\"[data-lists]\");\r\nconst newListForm = document.querySelector(\"[data-new-list-form]\");\r\nconst newListInput = document.querySelector(\"[data-new-list-input]\");\r\nconst deleteListButton = document.querySelector(\"[data-delete-list-button]\");\r\nconst listDisplayContainer = document.querySelector(\"[data-list-display-container]\");\r\nconst listTitleElement = document.querySelector(\"[data-list-title\");\r\nlet lists = JSON.parse(localStorage.getItem(\"task.lists\")) || [];\r\nlet selectedListId = localStorage.getItem(\"task.selectedListId\");\r\n\r\nfunction createList() {\r\n    return { id: Date.now().toString(), name: newListInput.value, tasks: [] };\r\n  }\r\n\r\n  newListForm.addEventListener(\"submit\", (e) => {\r\n    e.preventDefault();\r\n    const listName = newListInput.value;\r\n    if (listName === null || listName === \"\") return;\r\n    const list = createList();\r\n    newListInput.value = null;\r\n    lists.push(list);\r\n    renderAndSave();\r\n  });\r\n\r\n\r\n\r\nfunction renderLists() {\r\n  lists.forEach((list) => {\r\n    const listElement = document.createElement(\"li\");\r\n    listElement.innerText = list.name;\r\n    listElement.dataset.listId = list.id;\r\n    if (list.id === selectedListId) {\r\n      listElement.classList.add(\"active-list\");\r\n    }\r\n    listsContainer.appendChild(listElement);\r\n  });\r\n}\r\n\r\n\r\n\r\n\n\n//# sourceURL=webpack://todolist/./src/index.js?");
+eval("\n\n//# sourceURL=webpack://todolist/./src/index.js?");
 
 /***/ })
 
