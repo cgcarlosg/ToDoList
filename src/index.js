172c6e1482a7df6f87c0ae444c6d5fe517f66d15
carlos <cgcarlosg@hotmail.com>
2021-03-05 11:38:41 -0500

    Add function to display and mark as completed tasks

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 const listsContainer = document.querySelector('[data-lists')
 const newListForm = document.querySelector('[data-new-list-form]')
 const newListInput= document.querySelector('[data-new-list-input]')
+const deleteListButton = document.querySelector('[data-delete-list-button]')
+const listDisplayContainer = document.querySelector('[data-list-display-container]')
+const listTitleElement = document.querySelector('[data-list-title]')
+const listCountElement = document.querySelector('[data-list-count]')
+const tasksContainer = document.querySelector('[data-tasks]')
 
 const LOCAL_STORAGE_LIST_KEY = 'task.lists'
 const LOCAL_STORAGE_SELECTED_LIST_ID_KEY = 'task.selectedlistId'
@@ -10,7 +15,14 @@ let selectedListId = localStorage.getItem(LOCAL_STORAGE_SELECTED_LIST_ID_KEY)
 listsContainer.addEventListener('clilck', e => {
   if (e.target.tagName.toLowerCase() === 'li') {
     selectedListId = e.target.dataset.listId
+    saveAndRender()
   }
+})
+
+deleteListButton.addEventListener('click', e => {
+  lists = lists.filter(list => list.id !== selectedListId)
+  selectedListId = null
+  saveAndRender()
 })
 
 newListForm.addEventListener('submit', e => {
@@ -39,12 +51,35 @@ function saveAndRender() {
 
 function render() {
   clearElement(listsContainer)
+  renderLists()
+
+  const selectedList = lists.find(list => list.id === selectedListId)
+  if (selectedListId == null) {
+    listDisplayContainer.style.display = 'none'
+  } else {
+    listDisplayContainer.style.display = ''
+    listTitleElement.innerText = selectedList.name
+    renderTaskCount(selectedList)
+    clearElement(tasksContainer)
+    renderTasks(selectedList)
+  }
+}
+
+function renderTaskCount(selectedList) {
+  const incompleteTaskCount = selectedList.tasks.filter(task => !task.complete).length
+  const taskString = incompleteTaskCount === 1 ? "task" : "tasks"
+  listCountElement.innerText = `${incompleteTaskCount} ${taskString} remaining`
+}
+
+function renderLists() {
   lists.forEach(list => {
-    listElement.dataset.listId = list.id
     const listElement = document.createElement('li')
+    listElement.dataset.listId = list.id
     listElement.classList.add("list-name")
-    listElement.innerHTML = list.name
-    if (list.id === selectedListId) {listElement.classList.add('active-list')}
+    listElement.innerText = list.name
+    if (list.id === selectedListId) {
+      listElement.classList.add('active-list')
+    }
     listsContainer.appendChild(listElement)
   })
 }
